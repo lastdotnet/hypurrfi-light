094d2b6b6372414ccd211e8ab7c6b1b940b3ac36
alphak3y <alphak3y@protonmail.com>
2025-07-31 08:59:58 -0600

    update sim result

--- src/bindings/IPooledSimulator.sol
+++ src/bindings/IPooledSimulator.sol
@@ -8,12 +8,14 @@ interface IPooledSimulator {
         address user;
         uint256 debtToCover;
     }
+
     struct LiquidationSimulationResult {
-        uint256 collateralToLiquidate;
-        uint256 debtToRepay;
-        uint256 healthFactor;
-        bool isLiquidatable;
-        string reason;
+        uint256 collateralToLiquidate;      // Amount of collateral that would be liquidated (after protocol fee)
+        uint256 debtToRepay;                // Amount of debt that would be repaid (including flash loan fee)
+        uint256 debtTokensLeft;             // Amount of debt tokens left after repaying debt + flash loan fee (at oracle price)
+        uint256 healthFactor;               // User's current health factor
+        bool isLiquidatable;                // Whether the user can be liquidated
+        string reason;                      // Reason if not liquidatable
     }
 
     function ADDRESSES_PROVIDER() external view returns (address);
